79f78c64c42e0271537d942f7a5e4252ff02b3bb
huy2612 <97997396+huy2612@users.noreply.github.com>
2023-04-20 10:54:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,13 @@
 session_start();
 require("./Controllers/BaseController.php");
 require("./Core/DataBase.php");
+require("./Models/BaseModel.php");
+$controllerName=ucfirst(strtolower($_GET['controller'] ?? 'User')).'Controller';
+$action=$_GET["action"] ?? 'index';
+//echo $controllerName;
+require("./Controllers/${controllerName}.php");
 
+$controllerObject= new $controllerName;
+
+$controllerObject->$action();
 ?>
