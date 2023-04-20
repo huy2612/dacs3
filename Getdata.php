9f724e512e5254ef58fb7bacb27558ada10b05f7
huy2612 <97997396+huy2612@users.noreply.github.com>
2023-04-20 10:39:23 +0700

    Create Getdata.php

---
+++ Getdata.php
@@ -0,0 +1,20 @@
+<?php
+    // echo "android";
+    // $con=mysqli_connect("localhost", "root", "", "T");
+    // $kq=mysqli_query($con, "SELECT*FROM NGUOI");
+    // $data=array();
+    // while($row=mysqli_fetch_assoc($kq))
+    // {
+    //     array_push($data, $row);
+    // }
+    
+    // echo json_encode($data)
+
+    $t="dhh";
+    $data=[
+        $t => '124',
+    ];
+
+    print_r($data);
+    
+?>
